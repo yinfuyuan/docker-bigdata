4be7f25428c048cd28ec277ebe938ff495b004e2
yinfuyuan <yinfuyuan@gmail.com>
2018-06-08 14:37:23 +0800

    Add kafka service.

--- services/kafka/docker-entrypoint.sh
+++ services/kafka/docker-entrypoint.sh
@@ -2,19 +2,13 @@
 
 set -e
 
-# Allow the container to be started with `--user`
-if [[ "$1" = 'kafka-server-start.sh' && "$(id -u)" = '0' ]]; then
-    chown -R "$KAFKA_USER" "$KAFKA_DATA_DIR" "$KAFKA_DATA_LOG_DIR"
-    exec su-exec "$KAFKA_USER" "$0" "$@"
-fi
-
-# Generate the config only if it doesn't exist
+# Change the config only if it exist
 if [[ -f "$KAFKA_CONF_DIR/server.properties" ]]; then
     CONFIG="$KAFKA_CONF_DIR/server.properties"
-    sed -i s/"broker.id=0"/"broker.id=$BROKER_ID"/g "$CONFIG"
-    sed -i s/"#listeners=PLAINTEXT:\/\/:9092"/"listeners=PLAINTEXT:\/\/$LISTENERS"/g "$CONFIG"
-    sed -i s/"#advertised.listeners=PLAINTEXT:\/\/your.host.name:9092"/"advertised.listeners=PLAINTEXT:\/\/$ADVERTISED_LISTENERS"/g "$CONFIG"
-    sed -i s/"zookeeper.connect=localhost\:2181"/"zookeeper.connect=$ZOOKEEPER_CONNECT"/g "$CONFIG"
+    sed -i s/"broker.id=0"/"broker.id=${BROKER_ID:-0}"/g "$CONFIG"
+    sed -i s/"#listeners=PLAINTEXT:\/\/:9092"/"listeners=PLAINTEXT:\/\/${LISTENERS:-127.0.0.1:9092}"/g "$CONFIG"
+    sed -i s/"#advertised.listeners=PLAINTEXT:\/\/your.host.name:9092"/"advertised.listeners=PLAINTEXT:\/\/${ADVERTISED_LISTENERS:-127.0.0.1:9092}"/g "$CONFIG"
+    sed -i s/"zookeeper.connect=localhost:2181"/"zookeeper.connect=${ZOOKEEPER_CONNECT:-127.0.0.1:2181}"/g "$CONFIG"
 fi
 
-exec "$@"
+exec kafka-server-start.sh $KAFKA_CONF_DIR/server.properties
