a05442811661a263bffc521ec26b09f001ae489e
yinfuyuan <yinfuyuan@gmail.com>
2018-06-09 11:33:06 +0800

    Add hadoop 1.2

---
+++ services/hadoop/1.2/alpine/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+/usr/sbin/sshd
+
+hadoop namenode -format
+
+start-all.sh
+
+if [ "${1#-}" != "$1" ]; then
+	set -- hadoop "$@"
+fi
+
+if [ "$1" = 'hadoop-server' ]; then
+    kill $(pidof sshd)
+	/usr/sbin/sshd -D
+fi
+
+exec "$@"
