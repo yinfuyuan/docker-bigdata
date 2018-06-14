72691558b9337bb4b53b3679c1867f97c9cc7e53
yinfuyuan <yinfuyuan@gmail.com>
2018-06-14 15:43:05 +0800

    Change hbase config.

--- services/hbase/2.0/docker-entrypoint.sh
+++ services/hbase/2.0/docker-entrypoint.sh
@@ -7,18 +7,13 @@ if [[ -n "$CLUSTER" ]]; then
         sed -i s/"hdfs:\/\/localhost:9000"/"hdfs:\/\/$HDFS_FS_DEFAULTFS"/g "$HADOOP_CONF_DIR/core-site.xml"
     fi
     if [[ -n "$HDFS_SLAVES" ]]; then
-        echo $HDFS_SLAVES > "$HADOOP_CONF_DIR/slaves"
-        sed -i s/","/"\n"/g "$HADOOP_CONF_DIR/slaves"
+        echo -e "${HDFS_SLAVES/,/\\n}" > "$HADOOP_CONF_DIR/slaves"
     fi
     if [[ -n "$HDFS_DFS_REPLICATION" ]]; then
         sed -i s/"<value>1<\/value>"/"<value>$HDFS_DFS_REPLICATION<\/value>"/g "$HADOOP_CONF_DIR/hdfs-site.xml"
     fi
     if [[ -n "$HBASE_ROOTDIR" ]]; then
-        if [[ -n "$HBASE_ROOTDIR_HDFS" ]]; then
-            sed -i s/"hdfs:\/\/localhost:8020\/hbase"/"hdfs:\/\/$HBASE_ROOTDIR_HDFS\/$HBASE_ROOTDIR"/g "$HBASE_HOME/conf/hbase-site.xml"
-        else
-            sed -i s/"hdfs:\/\/localhost:8020\/hbase"/"$HBASE_ROOTDIR"/g "$HBASE_HOME/conf/hbase-site.xml"
-        fi
+        sed -i s/"hdfs:\/\/localhost:8020\/hbase"/"hdfs:\/\/${HBASE_ROOTDIR/\//\\/}"/g "$HBASE_HOME/conf/hbase-site.xml"
     fi
     if [[ -n "$HBASE_CLUSTER_DISTRIBUTED" ]]; then
         sed -i s/"<value>false<\/value>"/"<value>$HBASE_CLUSTER_DISTRIBUTED<\/value>"/g "$HBASE_HOME/conf/hbase-site.xml"
@@ -26,9 +21,11 @@ if [[ -n "$CLUSTER" ]]; then
     if [[ -n "$HBASE_ZOOKEEPER_QUORUM" ]]; then
         sed -i s/"<value>localhost<\/value>"/"<value>$HBASE_ZOOKEEPER_QUORUM<\/value>"/g "$HBASE_HOME/conf/hbase-site.xml"
     fi
-    if [[ -n "$HBASE_REGIONSERVERS" ]]; then
-        echo $HBASE_REGIONSERVERS > "$HBASE_HOME/conf/regionservers"
-        sed -i s/","/"\n"/g "$HBASE_HOME/conf/regionservers"
+    if [[ -n "$HBASE_REGION_SERVERS" ]]; then
+        echo -e "${HBASE_REGION_SERVERS/,/\\n}" > "$HBASE_HOME/conf/regionservers"
+    fi
+    if [[ -n "$HBASE_BACKUP_MASTER" ]]; then
+        echo -e "${HBASE_BACKUP_MASTER/,/\\n}" > "$HBASE_HOME/conf/backup-masters"
     fi
     echo "export HBASE_MANAGES_ZK=false" >> "$HBASE_HOME/conf/hbase-env.sh"
 fi
@@ -37,7 +34,9 @@ if [ "$1" = 'start-hbase.sh' ]; then
     /etc/init.d/ssh start
     hdfs namenode -format
     start-dfs.sh
-    start-hbase.sh
+    if [[ -z "$CLUSTER" || -n "$HBASE_MASTER" ]]; then
+        start-hbase.sh
+    fi
     tail -f /dev/null
 fi
 
