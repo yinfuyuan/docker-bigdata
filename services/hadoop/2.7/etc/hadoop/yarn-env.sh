3f5e53bd2d0897e96a7f26dd2c158e9507788bcc
yinfuyuan <yinfuyuan@gmail.com>
2018-06-19 13:25:32 +0800

    Change hadoop config.

--- services/hadoop/2.7/etc/hadoop/yarn-env.sh
+++ services/hadoop/2.7/etc/hadoop/yarn-env.sh
@@ -14,7 +14,8 @@
 # limitations under the License.
 
 # User for YARN daemons
-export HADOOP_YARN_USER=${HADOOP_YARN_USER:-yarn}
+# export HADOOP_YARN_USER=${HADOOP_YARN_USER:-yarn}
+export HADOOP_YARN_USER=hadoop
 
 # resolve links - $0 may be a softlink
 export YARN_CONF_DIR="${YARN_CONF_DIR:-$HADOOP_YARN_HOME/conf}"
