0484404e89c2016c7ed9a00576a5b84648b8876b
yinfuyuan <yinfuyuan@gmail.com>
2018-06-19 18:55:04 +0800

    Change kafka config.

--- services/kafka/1.1.0/docker-entrypoint.sh
+++ services/kafka/1.1.0/docker-entrypoint.sh
@@ -2,8 +2,26 @@
 
 set -e
 
+if [[ -z "$KAFKA_CLUSTER" || -z "$ZOOKEEPER_CONNECT" ]]; then
+    su-exec "$ZOO_USER" cp /conf/zoo_sample.cfg /conf/zoo.cfg
+    zkServer.sh start
+fi
+
+if [[ -n "$KAFKA_CLUSTER" ]]; then
+    sed -i "s/broker.id=.*/broker.id=${BROKER_ID:-0}/g" "/etc/kafka/server.properties"
+    if [[ -n "$LISTENERS" ]]; then
+        sed -i "s/listeners=.*/listeners=${LISTENERS////\\/}/g" "/etc/kafka/server.properties"
+    fi
+    if [[ -n "$ADVERTISED_LISTENERS" ]]; then
+        sed -i "s/advertised.listeners=.*/listeners=${ADVERTISED_LISTENERS////\\/}/g" "/etc/kafka/server.properties"
+    fi
+    if [[ -n "$ZOOKEEPER_CONNECT" ]]; then
+        sed -i "s/zookeeper.connect=.*/zookeeper.connect=$ZOOKEEPER_CONNECT/g" "/etc/kafka/server.properties"
+    fi
+fi
+
 if [ "$1" = 'kafka-server-start.sh' ]; then
-    kafka-server-start.sh /etc/kafka/server.properties
+    su-exec kafka kafka-server-start.sh /etc/kafka/server.properties
 fi
 
 exec "$@"
