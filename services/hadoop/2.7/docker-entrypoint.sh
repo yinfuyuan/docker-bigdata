f0ed3181129bf57dee33254be4443916e93ab4f8
yinfuyuan <yinfuyuan@gmail.com>
2018-06-28 16:45:17 +0800

    Adjust hadoop config and docker file.

--- services/hadoop/2.7/docker-entrypoint.sh
+++ services/hadoop/2.7/docker-entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 if [[ -n "${HDFS_FS_DEFAULTFS}" ]]; then
     sed -i "/<name>fs.defaultFS<\/name>/{n;s/<value>.*<\/value>/<value>${HDFS_FS_DEFAULTFS////\\/}<\/value>/g}" "${HADOOP_HOME}/etc/hadoop/core-site.xml"
 fi
-if [[ -n "${HDFS_SLAVES}" ]]; then
-    echo -e "${HDFS_SLAVES/,/\\n}" > "${HADOOP_HOME}/etc/hadoop/slaves"
+if [[ -n "${HADOOP_CLUSTER_SLAVES}" ]]; then
+    echo -e "${HADOOP_CLUSTER_SLAVES//,/\\n}" > "${HADOOP_HOME}/etc/hadoop/slaves"
     sed -i "/<name>dfs.replication<\/name>/{n;s/<value>.*<\/value>/<value>$(awk 'END{print NR}' ${HADOOP_HOME}/etc/hadoop/slaves)<\/value>/g}" "${HADOOP_HOME}/etc/hadoop/hdfs-site.xml"
 fi
 if [[ -n "${HDFS_DFS_REPLICATION}" ]]; then
@@ -15,6 +15,9 @@ fi
 if [[ -n "${HDFS_DFS_NAMENODE_SECONDARY_HTTP_ADDRESS}" ]]; then
     sed -i "/<name>dfs.namenode.secondary.http-address<\/name>/{n;s/<value>.*<\/value>/<value>${HDFS_DFS_NAMENODE_SECONDARY_HTTP_ADDRESS}<\/value>/g}" "${HADOOP_HOME}/etc/hadoop/hdfs-site.xml"
 fi
+if [[ -n "${HDFS_DFS_NAMENODE_SECONDARY_HTTPS_ADDRESS}" ]]; then
+    sed -i "/<name>dfs.namenode.secondary.https-address<\/name>/{n;s/<value>.*<\/value>/<value>${HDFS_DFS_NAMENODE_SECONDARY_HTTPS_ADDRESS}<\/value>/g}" "${HADOOP_HOME}/etc/hadoop/hdfs-site.xml"
+fi
 if [[ -n "${YARN_RESOURCEMANAGER_HOSTNAME}" ]]; then
     sed -i "/<name>yarn.resourcemanager.hostname<\/name>/{n;s/<value>.*<\/value>/<value>${YARN_RESOURCEMANAGER_HOSTNAME}<\/value>/g}" "${HADOOP_HOME}/etc/hadoop/yarn-site.xml"
 fi
@@ -22,13 +25,13 @@ fi
 if [ "$1" = 'start-all.sh' ]; then
     /etc/init.d/ssh start
     gosu hadoop /usr/local/hadoop/bin/hdfs namenode -format
-    if [[ -n "$HADOOP_CLUSTER_MASTER" || -n "$HDFS_CLUSTER_MASTER" ]]; then
+    if [[ ! -n "$HADOOP_CLUSTER_SLAVES" || -n "$HADOOP_CLUSTER_MASTER" || -n "$HDFS_CLUSTER_MASTER" ]]; then
         gosu hadoop /usr/local/hadoop/sbin/start-dfs.sh
     fi
-    if [[ -n "$HADOOP_CLUSTER_MASTER" || -n "$YARN_CLUSTER_MASTER" ]]; then
+    if [[ ! -n "$HADOOP_CLUSTER_SLAVES" || -n "$HADOOP_CLUSTER_MASTER" || -n "$YARN_CLUSTER_MASTER" ]]; then
         gosu hadoop /usr/local/hadoop/sbin/start-yarn.sh
     fi
-    if [[ -n "$HADOOP_CLUSTER_MASTER" || -n "$HISTORY_CLUSTER_MASTER" ]]; then
+    if [[ ! -n "$HADOOP_CLUSTER_SLAVES" || -n "$HADOOP_CLUSTER_MASTER" || -n "$HISTORY_CLUSTER_MASTER" ]]; then
         gosu hadoop /usr/local/hadoop/sbin/mr-jobhistory-daemon.sh start historyserver
     fi
     gosu hadoop tail -f /dev/null
