28f1a51ef21067b6aa96c271d904f4e634e05311
yinfuyuan <yinfuyuan@gmail.com>
2018-06-30 07:13:45 +0800

    Adjust hadoop config.

--- services/hadoop/2.7/docker-entrypoint.sh
+++ services/hadoop/2.7/docker-entrypoint.sh
@@ -15,11 +15,29 @@ fi
 if [[ -n "${HDFS_DFS_NAMENODE_SECONDARY_HTTP_ADDRESS}" ]]; then
     sed -i "/<name>dfs.namenode.secondary.http-address<\/name>/{n;s/<value>.*<\/value>/<value>${HDFS_DFS_NAMENODE_SECONDARY_HTTP_ADDRESS}<\/value>/g}" "${HADOOP_HOME}/etc/hadoop/hdfs-site.xml"
 fi
-if [[ -n "${HDFS_DFS_NAMENODE_SECONDARY_HTTPS_ADDRESS}" ]]; then
-    sed -i "/<name>dfs.namenode.secondary.https-address<\/name>/{n;s/<value>.*<\/value>/<value>${HDFS_DFS_NAMENODE_SECONDARY_HTTPS_ADDRESS}<\/value>/g}" "${HADOOP_HOME}/etc/hadoop/hdfs-site.xml"
+if [[ -n "${HDFS_DFS_DATANODE_ADDRESS}" ]]; then
+    sed -i "/<name>dfs.datanode.address<\/name>/{n;s/<value>.*<\/value>/<value>${HDFS_DFS_DATANODE_ADDRESS}<\/value>/g}" "${HADOOP_HOME}/etc/hadoop/hdfs-site.xml"
+fi
+if [[ -n "${HDFS_DFS_DATANODE_IPC_ADDRESS}" ]]; then
+    sed -i "/<name>dfs.datanode.ipc.address<\/name>/{n;s/<value>.*<\/value>/<value>${HDFS_DFS_DATANODE_IPC_ADDRESS}<\/value>/g}" "${HADOOP_HOME}/etc/hadoop/hdfs-site.xml"
 fi
 if [[ -n "${YARN_RESOURCEMANAGER_HOSTNAME}" ]]; then
     sed -i "/<name>yarn.resourcemanager.hostname<\/name>/{n;s/<value>.*<\/value>/<value>${YARN_RESOURCEMANAGER_HOSTNAME}<\/value>/g}" "${HADOOP_HOME}/etc/hadoop/yarn-site.xml"
+fi
+if [[ -n "${YARN_RESOURCEMANAGER_SCHEDULER_ADDRESS}" ]]; then
+    sed -i "/<name>yarn.resourcemanager.scheduler.address<\/name>/{n;s/<value>.*<\/value>/<value>${YARN_RESOURCEMANAGER_SCHEDULER_ADDRESS}<\/value>/g}" "${HADOOP_HOME}/etc/hadoop/yarn-site.xml"
+fi
+if [[ -n "${YARN_RESOURCEMANAGER_RESOURCE_TRACKER_ADDRESS}" ]]; then
+    sed -i "/<name>yarn.resourcemanager.resource-tracker.address<\/name>/{n;s/<value>.*<\/value>/<value>${YARN_RESOURCEMANAGER_RESOURCE_TRACKER_ADDRESS}<\/value>/g}" "${HADOOP_HOME}/etc/hadoop/yarn-site.xml"
+fi
+if [[ -n "${YARN_RESOURCEMANAGER_ADDRESS}" ]]; then
+    sed -i "/<name>yarn.resourcemanager.address<\/name>/{n;s/<value>.*<\/value>/<value>${YARN_RESOURCEMANAGER_ADDRESS}<\/value>/g}" "${HADOOP_HOME}/etc/hadoop/yarn-site.xml"
+fi
+if [[ -n "${YARN_RESOURCEMANAGER_ADMIN_ADDRESS}" ]]; then
+    sed -i "/<name>yarn.resourcemanager.admin.address<\/name>/{n;s/<value>.*<\/value>/<value>${YARN_RESOURCEMANAGER_ADMIN_ADDRESS}<\/value>/g}" "${HADOOP_HOME}/etc/hadoop/yarn-site.xml"
+fi
+if [[ -n "${YARN_NODEMANAGER_LOCALIZER_ADDRESS}" ]]; then
+    sed -i "/<name>yarn.nodemanager.localizer.address<\/name>/{n;s/<value>.*<\/value>/<value>${YARN_NODEMANAGER_LOCALIZER_ADDRESS}<\/value>/g}" "${HADOOP_HOME}/etc/hadoop/yarn-site.xml"
 fi
 
 if [ "$1" = 'start-all.sh' ]; then
@@ -34,7 +52,7 @@ if [ "$1" = 'start-all.sh' ]; then
     if [[ ! -n "$HADOOP_CLUSTER_SLAVES" || -n "$HADOOP_CLUSTER_MASTER" || -n "$HISTORY_CLUSTER_MASTER" ]]; then
         gosu hadoop /usr/local/hadoop/sbin/mr-jobhistory-daemon.sh start historyserver
     fi
-    gosu hadoop tail -f /dev/null
+    gosu hadoop sleep 8640h
 fi
 
 exec "$@"
